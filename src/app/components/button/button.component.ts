69c0bec118c399cb6cbdbc2407313e777ec559ef
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-08 19:29:33 -0300

    feat: emit btnClick event when btn tag is clicked

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -10,10 +10,15 @@ export class ButtonComponent implements OnInit {
   text!: string;
   @Input()
   color!: string | '#000';
+  @Output() btnClick = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClick() {
+    this.btnClick.emit();
   }
 
 }
