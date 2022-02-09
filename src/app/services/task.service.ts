4be41486ade667698b0498d8f5f5debd599b2d92
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 13:59:38 -0300

    feat: add service to deleteTask form the json-server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -15,4 +15,9 @@ export class TaskService {
     return this.http.get<Task[]>(this.apiUrl)
 
   }
+
+  deleteTask(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(url);
+  }
 }
