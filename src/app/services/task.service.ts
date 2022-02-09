01059c21c98504a3c41229207733ccd7631c93a6
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 13:35:20 -0300

    feat: remove unused imports

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from "@angular/common/http"
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Task } from '../Task'
 
 @Injectable({
