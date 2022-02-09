7bb0a1ad12a0f8e746d7999af801e4188a1f18f4
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 17:36:15 -0300

    feat: add connection to UiService inside toggleAddTask

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription } from 'rxjs';
+import { UiService } from '../../services/ui.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscription!: Subscription;
 
-  constructor() { }
+  constructor(private uiService:UiService) { 
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
   }
 }
