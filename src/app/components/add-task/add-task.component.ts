5d727f5a505d830ea56816795eb6f5d89da37d9b
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 16:03:02 -0300

    feat: emit onAddTask Event within onSubmit func

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Task } from '../../Task';
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
+
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  text!: string;
+  day!: string;
+  reminder: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (!this.text) {
+      alert('Please add  a Task!');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    };
+
+    this.onAddTask.emit(newTask);
+
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
 }
