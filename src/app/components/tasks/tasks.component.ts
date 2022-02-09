ee87b305215d9f22e5fbaa74dcdc1e1cf61f7c48
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 17:17:10 -0300

    feat: add addTask function that fires addTask function on services

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -35,4 +35,9 @@ export class TasksComponent implements OnInit {
       .subscribe();
   }
 
+  addTask(task: Task) {
+    this.taskService
+      .addTask(task)
+      .subscribe((task) => this.tasks.push(task))
+  }
 }
