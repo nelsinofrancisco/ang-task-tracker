bd6c307d5af33ec0da96274a8a70fda43c0a285b
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 13:21:45 -0300

    feat: subscribe to the observable getTasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../../Task'
-import { TASKS } from '../../mock-tasks'
+import { Task } from '../../Task';
+import { TaskService } from 'src/app/services/task.service';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
+
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => 
+    this.tasks = tasks);
   }
 
 }
