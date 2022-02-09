531f0a1ef5f75d58c493b860acfaf299c673cf3b
nelsinofrancisco <nelsino.francisco@gmail.com>
2022-02-09 13:34:36 -0300

    feat: add a http get request to get tasks from the json-server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from "@angular/common/http"
 import { Observable, of } from 'rxjs';
 import { Task } from '../Task'
-import { TASKS } from '../mock-tasks'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl)
+
   }
 }
